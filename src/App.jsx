439d69fa538654067be89f851771425abfab97a7
hyelim3 <shl0703ss@gmail.com>
2022-08-01 16:50:57 +0900

    todo 추가,불변성, 변화시 ui렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
+import Counter from "./components/Counter";
+import Converter from "./components/Converter";
+import MyBtn from "./components/MyBtn";
+
 function App() {
-  const [amount, setAmount] = useState(0);
-  const [active, setActive] = useState(true);
-
-  const onChange = (event) => {
-    console.log(event);
-
-    setAmount(event.target.value);
+  const [todos, setTodos] = useState([]); //할일목록
+  const [todo, setTodo] = useState(""); //할 일
+  const onChange = (e) => {
+    setTodo(e.target.value);
   };
+
+  const [counter, setCounter] = useState(0); //useState(0) 초기상태 0 counter에 영향
+  const [active, setActive] = useState(true);
+  // const onChange = (event) => {
+  // setMinutes(event.target.value);
+  //   console.log(event);
+  //   setCounter(event.target.value); //input 활성화
+  // };
   const reset = () => {
-    setAmount(0);
+    setCounter(0);
   };
   const changeActive = () => {
     reset();
@@ -18,24 +27,71 @@ function App() {
     });
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (todo === "") {
+      alert("1글자 이상 입력해주세요");
+      return;
+      // 밑에 실행 안됨 끝남
+    }
+    setTodos((currentArray) => [todo, ...currentArray]);
+    setTodo("");
+  };
+
   return (
-    <div>
+    <>
+      <div>
+        <h1>할 일</h1>
+        <form onSubmit={onSubmit}>
+          {/* //제출할 대 함수 실행 */}
+          <input
+            type="text"
+            onChange={onChange}
+            value={todo}
+            placeholder="할 일을 적어주세요."
+          />
+          <button
+            onClick={() => {
+              console.log(todos);
+            }}
+          >
+            등록
+          </button>
+        </form>
+
+        <button
+          onClick={() => {
+            console.log(todos);
+          }}
+        >
+          check
+        </button>
+        <hr />
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+        <hr />
+      </div>
+      <Counter counter={counter} setCounter={setCounter} />
+      <hr />
       <div>Time Converter</div>
       <div>
         Minutes{" "}
         <input
-          placeholder="Minutes"
-          value={active ? amount : amount * 60}
           type="number"
+          placeholder="Minutes"
+          value={active ? counter : counter * 60}
           onChange={onChange}
-          disabled={!active}
+          disabled={!active} //비활성화=true 활성화가 되려면 false
         />
         <div>
           Hours{" "}
           <input
             type="number"
             placeholder="Hours"
-            value={active ? Math.floor(amount / 60) : amount}
+            value={active ? Math.floor(counter / 60) : counter}
             disabled={active}
             onChange={onChange}
           />
@@ -44,7 +100,7 @@ function App() {
         <button onClick={changeActive}>active toggle</button>
       </div>
       <hr />
-    </div>
+    </>
   );
 }
 
