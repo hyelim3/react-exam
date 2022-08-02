1a7db99f77a2ac60307e01d73fdf39c4127224de
hyelim3 <shl0703ss@gmail.com>
2022-08-02 18:40:35 +0900

    할 일 todo checked

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
 import React, { useRef, useState } from "react";
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
   const nextId = useRef(1);
   const onInsert = (text) => {
     const todo = {
-      id: nextId,
+      id: nextId.current,
       text: text,
       checked: false,
     };
     setTodos((todos) => todos.concat(todo));
     nextId.current++;
   };
-
+  const onToggle = (id) => {
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <button
-        onClick={() => {
-          console.log(todos);
-        }}
-      >
-        show todos
-      </button>
+      <TodoList todos={todos} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
+
 export default App;
