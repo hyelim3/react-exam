6b3fef2cbe44ada94464697dff8421152962bb13
hyelim3 <shl0703ss@gmail.com>
2022-08-03 16:20:30 +0900

    todolist 리뉴얼 todo 수정화면1부

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useRef, useState } from "react";
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
+import TodoEdit from "./components/TodoEdit";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
+  const [insertToggle, setInsertToggle] = useState(false);
   const nextId = useRef(1);
   const onInsert = (text) => {
     const todo = {
@@ -16,6 +18,7 @@ const App = () => {
     nextId.current++;
   };
   const onToggle = (id) => {
+    //눌렀을 때 어떤 일이 일어나는지
     setTodos((todos) =>
       todos.map((todo) =>
         todo.id === id ? { ...todo, checked: !todo.checked } : todo
@@ -23,14 +26,24 @@ const App = () => {
     );
   };
 
+  const onInsertToggle = () => {
+    setInsertToggle((prev) => !prev);
+  }; //이전 상태 prev
+
   const onRemove = (id) => {
     setTodos((todos) => todos.filter((todo) => todo.id !== id)); //setTodo받아옴 돌다가 id가 같으면 삭제, filter 걸리는 것만 남김
-  };
+  }; //setTodos를 수정 -> todos를 불러와서 todos에 있는 todo 중에 id값이 다르면 남아있어!
 
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onToggle={onToggle} onRemove={onRemove} />
+      <TodoList
+        todos={todos}
+        onToggle={onToggle}
+        onRemove={onRemove}
+        onInsertToggle={onInsertToggle}
+      />
+      {insertToggle && <TodoEdit onInsertToggle={onInsertToggle} />}
     </TodoTemplate>
   );
 };
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 import "../styles/TodoList.scss";
 
-const TodoList = ({ todos, onToggle, onRemove }) => {
+const TodoList = ({ todos, onToggle, onRemove, onInsertToggle }) => {
   return (
     <ul className="TodoList">
       {todos.map((todo, index) => (
         <TodoListItem
           todo={todo}
           key={index}
-          onToggle={onToggle}
           onRemove={onRemove}
+          onToggle={onToggle}
+          onInsertToggle={onInsertToggle}
         />
       ))}
     </ul>
